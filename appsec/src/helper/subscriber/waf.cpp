e85757bdc58a22a1abd1f6736fb17466f6c97fe6
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2024-10-02 11:45:00 +0200

    Lint

--- appsec/src/helper/subscriber/waf.cpp
+++ appsec/src/helper/subscriber/waf.cpp
@@ -233,6 +233,7 @@ void instance::listener::call(
     // NOLINTNEXTLINE
     total_runtime_ += res.total_runtime / 1000.0;
     if (rasp) {
+        // NOLINTNEXTLINE
         rasp_runtime_ += res.total_runtime / 1000.0;
         rasp_calls_++;
         if (res.timeout) {
